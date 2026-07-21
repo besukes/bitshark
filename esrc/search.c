96cf04da537f896d482a902829b1648f26f6586b
besukes <besukesfn@gmail.com>
2026-07-22 00:40:20 +0100

    small fixes

--- esrc/search.c
+++ esrc/search.c
@@ -6,7 +6,7 @@ int depth_search(GameStruct * game , SearchInfo * search_info , int cur_depth){
     int cur_alpha = evaluate_pos(game,search_info,0) ,
         cur_beta = evaluate_pos(game,search_info,1)  ;
     if(cur_depth >= search_info->depth || cur_alpha < search_info->alpha || cur_beta > search_info->beta) 
-        return ((search_info->turn == brancas) ? cur_alpha : cur_beta);
+        return ((search_info->turn == brancas) ? cur_alpha : cur_beta); 
     int new_turn = (search_info->turn == brancas) ? pretas : brancas;
     return move_algorithm(game,new_turn,search_info->depth-1,search_info->ai_level,cur_alpha,cur_beta).move_evaluation;
     //necessita de verificar o turno atual e aplicar a melhor jogada , decrementando o turno e fazendo recursividade para procurar
@@ -22,7 +22,6 @@ Moves search_algorithm (uint64_bit atks , uint64_bit pos, GameStruct * game ,Pie
         if(atks & 1ULL){
             casa_atual = 1ULL<<cntr;
             atualizaJogada(game,casa_atual,0,0);
-            search_info->turn = (search_info->turn == brancas) ? pretas : brancas;
             int new_eval = depth_search(game,search_info,0);
             //quando o turno troca , se chegarmos ao fim da depth estamos a conseguir a evaluation da jogada que fizemos , com o turno ja trocado
             if(new_eval > search_info->alpha && search_info->turn == brancas){
