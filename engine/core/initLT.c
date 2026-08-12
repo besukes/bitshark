4cfa9958392876812013ec7b2971aa89d4a9fd24
besukes <besukesfn@gmail.com>
2026-08-12 10:05:23 +0100

    Fix loop initialization for jogadas in initLT

--- engine/core/initLT.c
+++ engine/core/initLT.c
@@ -3,7 +3,7 @@
 
 void init_lmrLT_table(){
     for(int depth=1;depth<MAX_DEPTH_SEARCH;depth++){
-        for(int jogadas=0;jogadas<MAX_NUMBER_MOVES;jogadas++){
+        for(int jogadas=1;jogadas<MAX_NUMBER_MOVES;jogadas++){
             lmr_lt[depth][jogadas] = (int)( 0.6 + (log(depth)*log(jogadas))/1.6);
         }
     }
